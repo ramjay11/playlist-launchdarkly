9f69f0d16d5d05cc5ffccad746dcd2af2cefd142
Ramjay Romorosa <ramjayromorosa@yahoo.com>
2022-01-15 02:23:06 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  // Hard coded data
+  constructor() {
+    super()
+    this.state = {
+      songs: [
+        {name: 'Swasti-ska', added: '2022-01-15'},
+        {name: 'Auftrag Deutsches Reich', added: '2022-01-14'},
+      ]
+    }
+  }
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {this.state.songs.map(song =>
+              <li>{song.name}</li>
+          )}
+        <li>Title 1</li>
+        <li>Title 2</li>
+        <li>Title 3</li>
+      </ul>
     </div>
   );
+  }
 }
 
 export default App;
